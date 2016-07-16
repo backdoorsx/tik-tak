42d3653dd56c26ec0b6ba024593bb18717cd8100
Arch Linux <root@localhost.localhost>
2016-07-17 00:43:09 +0200

    final

--- tik-tak.c
+++ tik-tak.c
@@ -9,5 +9,24 @@ int main(){
 		scanf("%d", &number);
 		printf("You have entered: %d\n", number);
 
+		if( number > 0 && number < 10000 )
+				;
+		else{
+				printf("-1\n");
+				return 1;
+		}
+		
+
+		for( int i=1; i<number+1; i++  ){
+			if( (i%3 == 0) && (i%5 == 0) )
+					printf("tiktak\n");
+			else if( i%3 == 0 )
+					printf("tik\n");
+			else if( i%5 == 0 )
+					printf("tak\n");
+			else
+					printf("%d\n", i);
+		}
+
 		return 0;
 }
